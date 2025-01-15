8741cb296ebd90be564c5cbb43739b621e44b2d5
Chris Povirk <cpovirk@google.com>
2025-01-15 14:19:40 -0500

    Add `@NullMarked` to `Transformer`.

--- subprojects/core-api/src/main/java/org/gradle/api/Transformer.java
+++ subprojects/core-api/src/main/java/org/gradle/api/Transformer.java
@@ -15,6 +15,8 @@
  */
 package org.gradle.api;
 
+import org.jspecify.annotations.NullMarked;
+
 /**
  * <p>A {@code Transformer} transforms objects of type.</p>
  *
@@ -23,6 +25,7 @@ package org.gradle.api;
  * @param <OUT> The type the value is transformed to.
  * @param <IN> The type of the value to be transformed.
  */
+@NullMarked
 public interface Transformer<OUT extends @org.jetbrains.annotations.Nullable Object, IN> {
     /**
      * Transforms the given object, and returns the transformed value.
